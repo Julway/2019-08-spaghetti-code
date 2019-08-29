9c98f164e80c951423c9391b421a9c963f5d640d
Julway <anna_gorny@hotmail.com>
2019-08-29 15:45:10 +0200

    refactored: main($argc, $argv);

--- weekday.php
+++ weekday.php
@@ -1,10 +1,7 @@
 <?php
-
 /**
  * Wochentagsberechnung nach https://de.wikipedia.org/wiki/Wochentagsberechnung
  */
-
-setlocale(LC_TIME, 'de_AT.utf-8');
 
 /**
  * @param int $w
@@ -61,31 +58,39 @@ function printDebugOutput(int $m, $y, $c): void
     }
 }
 
-$day = $argv[1];
-$month = $argv[2];
-$year = $argv[3]; /* muss vierstellig sein */
+function main(int $argc, array $argv): void
+{
 
-if($argc<4 || $argc>5) {
-    echo "Wrong number of arguments.";
-    exit(1);
+    setlocale(LC_TIME, 'de_AT.utf-8');
+
+    $day = $argv[1];
+    $month = $argv[2];
+    $year = $argv[3]; /* muss vierstellig sein */
+
+    if ($argc < 4 || $argc > 5) {
+        echo "Wrong number of arguments.";
+        exit(1);
+    }
+
+    $m = (($month - 2 - 1) + 12) % 12 + 1; // this is because of the modulo
+    $c = substr($year, 0, 2);
+    if ($m >= 11) {
+        $c = substr($year - 1, 0, 2);
+    }
+    $y = substr($year, 2, 2);
+    if ($m >= 11) {
+        $y = substr($year - 1, 2, 2);
+    }
+
+    $weekdayNumber = ($day + intval(2.6 * $m - 0.2) + $y + intval($y / 4) + intval($c / 4) - 2 * $c) % 7;
+
+    $weekday = getWeekdayName($weekdayNumber);
+
+    printEingabe($day, $month, $year);
+
+    printAusgabe($year, $month, $day, $weekday);
+
+    printDebugOutput($m, $y, $c);
 }
 
-$m = (($month - 2 - 1 ) + 12 ) % 12 + 1 ; // this is because of the modulo
-$c = substr($year, 0, 2);
-if($m>=11) {
-    $c = substr($year-1, 0, 2);
-}
-$y = substr($year, 2, 2);
-if($m>=11) {
-    $y = substr($year-1, 2, 2);
-}
-
-$weekdayNumber = ($day + intval (2.6 * $m - 0.2) + $y  + intval ($y/4) + intval ($c/4) - 2*$c ) % 7;
-
-$weekday = getWeekdayName($weekdayNumber);
-
-printEingabe($day, $month, $year);
-
-printAusgabe($year, $month, $day, $weekday);
-
-printDebugOutput($m, $y, $c);
+main($argc, $argv);
